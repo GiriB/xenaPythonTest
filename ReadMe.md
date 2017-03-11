a4e127c478060027b759769e0e6488f76c610cbf
Giri Bhatnagar <Girigaurav_bhatnagar@intuit.com>
2017-03-11 23:10:28 +0530

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+##xenaPythonTest
+This is testpypi package for [xenaAPI](https://github.com/jingchunzhu/cgDataNew/tree/master/xena) 
+
+The package can be installed as
+ 
+    pip install -i https://testpypi.python.org/pypitest xenaPythonTest 
+
+Use as 
+
+    >>from xenaPythonTest import xenaAPI
+ 
+ OR
+ 
+    >>import xenaPythonTest as xena
+    >>xena.xenaAPI                
+    
+
+
+###Packaging 
+####Installation 
+    pip install twine
+####Run
+    python setup.py sdist bdist_wheel           #build
+    twine register -r pypitest dist/*           #register 
+    twine upload -r pypitest dist/*             #upload 
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+print(find_packages())
+setup(
+    name="xenaPythonTest",
+    version="0.2.1",
+    packages=find_packages(),
+    author = 'GiriB',
+    author_email = 'giri1gb@gmail.com',
+    description = 'Test package for XENA API',
+    url = 'https://github.com/jingchunzhu/cgDataNew/tree/master/xena', 
+    keywords = ['xena', 'ucsc', 'xenaAPI'],
+    license='MIT', 
+)
+
+
+# setup(
+#   name = 'xenaPythonTest',
+#   packages = ['xenaPythonTest'], # this must be the same as the name above
+#   version = '0.1',
+#   description = 'A random test lib',
+#   maintainer = 'GiriB',
+#   maintainer_email = 'giri1gb@gmail.com',
+#   url = 'https://github.com/jingchunzhu/cgDataNew/tree/master/xena', 
+#   keywords = ['xena', 'ucsc', 'xenaAPI'], 
+# )
